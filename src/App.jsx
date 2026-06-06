52fb7b2f626eb9c9fcbe283107b4fc4eea80aa24
Himu Mutte <himu.mutte5@gmail.com>
2026-06-06 13:16:01 +0530

    Implemented RectSortingStrategy

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import ListItem  from "./components/ListItem"
 import { SortableContext , arrayMove } from "@dnd-kit/sortable"
 
 import { DndContext, closestCorners , useSensor , useSensors , MouseSensor , KeyboardSensor , TouchSensor } from "@dnd-kit/core"
-import { sortableKeyboardCoordinates } from "@dnd-kit/sortable"
+import { sortableKeyboardCoordinates, rectSortingStrategy } from "@dnd-kit/sortable"
 
 import { useState } from "react"
 
 const items = [
   {id:1, title:"Wash Clothes"},
   {id:2, title:"Play Games"},
-  {id:3, title:"Eat Food"}
+  {id:3, title:"Eat Food"},
+  {id:4, title:"Go to Gym"},
 ]
 
 const App = () =>{
@@ -40,8 +41,8 @@ const App = () =>{
     <div className="min-h-screen bg-indigo-500 p-5">
       <h1 className="text-white text-center text-3xl font-bold">To Do List</h1>
       <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd} sensors={sensors}>
-        <SortableContext items={listItems} >
-          <ul className="flex flex-col gap-5 bg-white p-5 mt-5 rounded-md max-w-[600px] m-auto shadow-xl">
+        <SortableContext items={listItems} strategy={rectSortingStrategy}>
+          <ul className="flex flex-col gap-5 bg-white p-5 mt-5 rounded-md max-w-[700px] m-auto shadow-xl grid grid-cols-2 gap-4">
             {listItems.map(item=>{
               return (
                   <ListItem data={item}/>
